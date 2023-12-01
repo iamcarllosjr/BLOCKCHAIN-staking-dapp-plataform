07abe9216181391971b062075386c5b77746d2d3
Carlos <iamcarllosjr@gmail.com>
2023-12-01 15:00:01 -0300

    Reward Rate Token page created

--- src/app/components/Display Pannel/RewardRate.jsx
+++ src/app/components/Display Pannel/RewardRate.jsx
@@ -1,7 +1,34 @@
-
+"use client";
+import { useState, useEffect, useContext } from "react";
+import Web3Context from "@/app/Context/Web3Context";
+import { ethers } from "ethers";
 
 const RewardRate = () => {
+    const { stakingContractInstance, selectedAccount } = useContext(Web3Context);
+    const [rewardRate, setRewardRate] = useState("0");
+
+    useEffect(() => {
+        const fetchRewardRate = async () => {
+            try {
+                const rewardTokenRate = await stakingContractInstance.REWARD_RATE();  
+                const convertWeiToEther = ethers.formatUnits(rewardTokenRate.toString(), 18);
+                console.log(convertWeiToEther);
+                setRewardRate(convertWeiToEther);
+            } catch (error) {
+                console.error("Error Fetching Data...", error.message)   
+            };
+
+        }
+        stakingContractInstance && fetchRewardRate();
+
+    }, [stakingContractInstance, selectedAccount]);
+
+    return (
+        <div>
+            <p>Reward Rate : {rewardRate} /seconds</p>
+        </div>
+    )
  
 }
 
-export default RewardRate
+export default RewardRate;
