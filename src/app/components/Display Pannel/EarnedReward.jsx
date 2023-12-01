a6b1a17e4f066474e53e87062e5617edfff38aa6
Carlos <iamcarllosjr@gmail.com>
2023-12-01 15:13:00 -0300

    Created page to show reward value

--- src/app/components/Display Pannel/EarnedReward.jsx
+++ src/app/components/Display Pannel/EarnedReward.jsx
@@ -1,6 +1,34 @@
-
+"use client";
+import { useState, useEffect, useContext } from "react";
+import Web3Context from "@/app/Context/Web3Context";
+import { ethers } from "ethers";
 
 const EarnedReward = () => {
+    const { stakingContractInstance, selectedAccount } = useContext(Web3Context);
+    const [earnedReward, setEarnedReward] = useState("0");
+
+    useEffect(() => {
+        const fetchEarnedReward = async () => {
+            try {
+                const earnedTokenReward = await stakingContractInstance.earned(selectedAccount);  
+                const convertWeiToEther = ethers.formatUnits(earnedTokenReward.toString(), 18);
+                const fixedValueReward = parseFloat(convertWeiToEther).toFixed(2)
+                console.log(fixedValueReward);
+                setEarnedReward(fixedValueReward);
+            } catch (error) {
+                console.error("Error Fetching Data...", error.message)   
+            };
+
+        }
+        stakingContractInstance && fetchEarnedReward();
+
+    }, [stakingContractInstance, selectedAccount]);
+
+    return (
+        <div>
+            <p>Earned Reward : {earnedReward}</p>
+        </div>
+    )
   
 }
 
